97d9368eb919ca6fcb9a03138b9a1bb1f6fd2ee5
Helgi Da Canzaparca <lapotnik1981@gmail.com>
2020-10-20 12:21:00 +0300

    Добавил main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+#include </home/helgi/git_repo/Binaural/input_file.h>
+
+using namespace std;
+
+
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
